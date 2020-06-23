915b081da5f63479ec460619597bcd5370d1916f
Dima Gusev <62891657+qwertyqwe55@users.noreply.github.com>
2020-06-23 23:34:14 +0300

    Update bilets.h

--- bilets.h
+++ bilets.h
@@ -1,7 +1,7 @@
 #ifndef BILET_H
 #define BILET_H
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 class Bilet {
 public:
@@ -15,20 +15,29 @@ public:
     string GetSecond();
     Bilet * next();
 
+    friend ostream& operator<< (ostream &out, Bilet &student);
+	friend istream& operator>> (istream &in, Bilet &student);
+	friend ofstream& operator<< (ofstream &fout, Bilet &student);
+	friend ifstream& operator>> (ifstream &fin, Bilet &student);
     void setDay(int day);
+    void print();
     void setMonth(string month);
     void setPrice(int price);
     void setFirst(string first_place);
     void setSecond(string second_place);
     void setNext(Bilet * next);
 
-private:
+
     int _day;
     string _month; 
     int _price; 
     string _first_place; 
     string _second_place;
+
+private:
+
     Bilet * _next;
 };
+
 
 #endif // TEST_H
