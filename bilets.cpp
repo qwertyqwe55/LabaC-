b4b29aa9b5899ef46ba352cf8d05e460826ef510
Dima Gusev <62891657+qwertyqwe55@users.noreply.github.com>
2020-06-23 23:34:12 +0300

    Update bilets.cpp

--- bilets.cpp
+++ bilets.cpp
@@ -1,4 +1,6 @@
 #include "bilets.h"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 Bilet::Bilet(){
@@ -18,6 +20,7 @@ Bilet::Bilet(int day,string month, int price, string first_place, string second_
         _second_place = second_place;
         _next = NULL;
     }
+
 
 int Bilet::GetDay(){
     return _day;
@@ -67,4 +70,50 @@ void Bilet:: setFirst(string first_place){
 void Bilet:: setSecond(string second_place){
     if(_second_place != second_place)
         _second_place = second_place;
+}
+
+void print(Bilet& bilet){
+    cout << "День: " << bilet.GetDay() << endl;
+	cout << "Месяц: " << bilet.GetMonth() << endl;
+    cout << "Цена: " << bilet.GetPrice() << endl;
+    cout << "Место отбытия:" << bilet.GetFirst() << endl;
+	cout << "Место прибытия: " << bilet.GetSecond() << endl;
+}
+ostream& operator<< (ostream &out, Bilet &bilet){
+	out << "День: " << bilet.GetDay() << endl;
+	out << "Месяц: " << bilet.GetMonth() << endl;
+    out << "Цена: " << bilet.GetPrice() << endl;
+    out << "Место отбытия:" << bilet.GetFirst() << endl;
+	out << "Место прибытия: " << bilet.GetSecond() << endl;
+	return out;
+}
+istream& operator>> (istream &in, Bilet &bilet){
+	cout << "День: ";
+    in >> bilet._day;
+	cout << "Месяц: ";
+	in >> bilet._month;
+	cout << "Цена: ";
+    in >> bilet._price;
+	cout << "Место отбытия: ";
+	in >> bilet._first_place;
+	cout << "Место прибытия: ";
+	in >> bilet._second_place;
+
+    return in;	
+}
+ofstream& operator<< (ofstream &fout, Bilet &bilet){
+	fout << bilet._day << endl;
+	fout << bilet._month << endl;
+    fout << bilet._price << endl;
+	fout << bilet._first_place << endl;
+	fout << bilet._second_place << endl;
+    return fout;	
+}
+ifstream& operator>> (ifstream &fin, Bilet &bilet){
+	fin >> bilet._day;
+	fin >> bilet._month;
+    fin >> bilet._price;
+	fin >> bilet._first_place;
+	fin >> bilet._second_place;
+    return fin;	
 }
